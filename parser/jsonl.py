63a6c1ed0333e2fac0d056ea36c7cea92854d57a
Fabrice Livigni <>
2024-07-02 18:34:31 +0200

    last commit FL

--- parser/jsonl.py
+++ parser/jsonl.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_json('ftdata.jsonl', lines=True)
+df = pd.read_json('ftdata1.jsonl', lines=True)
 print(df)
 for row in df.itertuples(index=True, name='Pandas'):
     print(f"Index: {row.Index}")
